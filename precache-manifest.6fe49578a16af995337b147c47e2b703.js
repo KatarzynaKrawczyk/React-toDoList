1304dd4bae5f36a75702b47d98d2cb98e383e0c4
KatarzynaKrawczyk <krawczyk.kat@gmail.com>
2019-01-21 21:49:45 +0100

    Updates

---
+++ precache-manifest.6fe49578a16af995337b147c47e2b703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c1f2e4c0c7e62f0706",
+    "url": "/React-toDoList/static/js/runtime~main.46c1f2e4.js"
+  },
+  {
+    "revision": "5c00ae0e2a5a3e561134",
+    "url": "/React-toDoList/static/js/main.5c00ae0e.chunk.js"
+  },
+  {
+    "revision": "5df0fe6d252843260191",
+    "url": "/React-toDoList/static/js/1.5df0fe6d.chunk.js"
+  },
+  {
+    "revision": "5c00ae0e2a5a3e561134",
+    "url": "/React-toDoList/static/css/main.320ac2df.chunk.css"
+  },
+  {
+    "revision": "5df0fe6d252843260191",
+    "url": "/React-toDoList/static/css/1.b1398381.chunk.css"
+  },
+  {
+    "revision": "9f648de03a63f5da6c1cb5cdd7c546b4",
+    "url": "/React-toDoList/index.html"
+  }
+];
